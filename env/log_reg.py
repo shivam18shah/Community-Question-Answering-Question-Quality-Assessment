3a42c5e6032faa421929ced1a5b6c44fd0f35a5f
shivam18shah <spshah18@asu.edu>
2019-11-03 02:01:15 -0700

    Update log_reg.py
    Cross validation included

--- env/log_reg.py
+++ env/log_reg.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.model_selection import train_test_split
-# from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import pickle
@@ -15,17 +14,21 @@ OUTPUT_FILE = 'log_reg.obj'
 def log_reg():
     df = pd.read_csv(CSV_FILE)
     # print(df.head())
-    train_df = df.iloc[:,:-1]
-    test_df = df.iloc[:,-1]
-
-    X_train, X_test, y_train, y_test = train_test_split(train_df, test_df, test_size=0.3, random_state=10)
+    X_df = df.iloc[:,:-1]
+    y_df = df.iloc[:,-1]
 
     log_reg = LogisticRegression()
-    log_reg.fit(X_train, y_train)
 
-    pred = log_reg.predict(X_test)
-    print(classification_report(y_test, pred))
-    coeffs = log_reg.coef_
+    # without cross validation, requires change in order
+    # X_train, X_test, y_train, y_test = train_test_split(X_df, y_df, test_size=0.3, random_state=10)
+    # log_reg.fit(X_train, y_train)
+    # pred = log_reg.predict(X_test)
+    # print(classification_report(y_test, pred))
+    # coeffs = log_reg.coef_
+
+    # with cross validation
+    evals = cross_validate(log_reg, train_df, test_df, cv=3)
+    print(evals)
     pickle.dump(coeffs, OUTPUT_FILE)
 
 if __name__==__main__:

05bd95b8f418a390e0d3b4566945eb9deed48242
shivam18shah <spshah18@asu.edu>
2019-11-08 18:45:09 -0700

    Update log_reg.py

--- log_reg.py
+++ log_reg.py
@@ -11,14 +11,13 @@ import warnings
 warnings.filterwarnings('ignore')
 
 CSV_FILE = os.path.join('./dataset','train.csv')
-# OUTPUT_FILE = os.path.join('./Outputs','log_reg.obj')
 OUTPUT_FOLDER = './Outputs'
 OUTPUT_FILE = os.path.join(OUTPUT_FOLDER, 'log_reg_results.obj')
 MODEL_FILE = os.path.join(OUTPUT_FOLDER, 'log_reg_model.obj')
 
 def log_reg():
     df = pd.read_csv(CSV_FILE)
-    print(df.head())
+    # print(df.head())
     df.dropna(inplace=True)
     X_df = df.iloc[:,:-1]
     y_df = df.iloc[:,-1]
@@ -26,11 +25,14 @@ def log_reg():
     # print(y_df.head())
     log_reg = LogisticRegressionCV(cv=5, random_state=10).fit(X_df, y_df) #Cs=4, fit_intercept=True, cv=10, verbose =1, random_state=42)
 
-    print(X_df.shape, y_df.shape)
+    # print(X_df.shape, y_df.shape)
     # with cross validation
     evals = cross_validate(log_reg, X_df, y_df, cv=5)
     print(evals)
     coeffs = log_reg.coef_
+    scores = log_reg.scores_
+    print('Scores:',scores)
+    print(len(i) for i in scores)
     with open(OUTPUT_FILE, 'wb') as f:
         pickle.dump(coeffs, f)
     f.close()

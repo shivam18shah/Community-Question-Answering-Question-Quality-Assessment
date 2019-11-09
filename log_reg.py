3610da9d7299618edc1c9a9e6090b95963ce7fe4
shivam18shah <spshah18@asu.edu>
2019-11-08 19:03:18 -0700

    svm added

--- log_reg.py
+++ log_reg.py
@@ -17,21 +17,21 @@ MODEL_FILE = os.path.join(OUTPUT_FOLDER, 'log_reg_model.obj')
 
 def log_reg():
     df = pd.read_csv(CSV_FILE)
-    # print(df.head())
-    df.dropna(inplace=True)
+    df = df.reset_index()
+    # df.dropna(inplace=True)
+    df = df[ ~df.isin([np.nan, np.inf, -np.inf]).any(1) ]
     X_df = df.iloc[:,:-1]
     y_df = df.iloc[:,-1]
-    # print(X_df.head())
-    # print(y_df.head())
+    # np.where(X_df.values >= np.finfo(np.float64).max)
+    # print(len(df))
     log_reg = LogisticRegressionCV(cv=5, random_state=10).fit(X_df, y_df) #Cs=4, fit_intercept=True, cv=10, verbose =1, random_state=42)
-
+    
     # print(X_df.shape, y_df.shape)
     # with cross validation
     evals = cross_validate(log_reg, X_df, y_df, cv=5)
     print(evals)
     coeffs = log_reg.coef_
     scores = log_reg.scores_
-    print('Scores:',scores)
     print(len(i) for i in scores)
     with open(OUTPUT_FILE, 'wb') as f:
         pickle.dump(coeffs, f)

9938d16cd7b19cd5eb2c2b9679673c98e8029f99
shivam18shah <spshah18@asu.edu>
2019-11-09 02:12:16 -0700

    svm outputs here

--- svm.py
+++ svm.py
@@ -32,24 +32,25 @@ def svm_reg():
 
     _ = input('Before you run, make sure that the previous execution models have been stored safely, as the files will be overwritten. Enter any input to continue, or terminate the execution and securely save the previous model: ')
 
-    lsvm = svm.SVC(kernel='linear',C = 1.0, random_state=10).fit(X_df, y_df) #Cs=4, fit_intercept=True, cv=10, verbose =1, random_state=42)
+    lsvm = svm.SVC(kernel='sigmoid',C = 1.0, tol = 1e-3, random_state=10).fit(X_df, y_df) #Cs=4, fit_intercept=True, cv=10, verbose =1, random_state=42)
     
     
     print(X_df.shape, y_df.shape)
     #print(summary(lsvm))
     # with cross validation
     evals = cross_val_score(lsvm, X_df, y_df, cv=5)
-    
-    coeffs = svm.coef_
-    scores = svm.scores_
+    # coeffs = lsvm.coef_ # for kernel='linear' only
+    # scores = lsvm.scores_
+    supp_vecs = lsvm.support_vectors_
     print('Eval:', evals)
-    print('Coeffs: ', coeffs)
-    print('Scores: ', scores)
+    # print('Coeffs: ', coeffs)
+    # print('Scores: ', scores)
+    print('Support Vectors: ', supp_vecs)
     with open(OUTPUT_FILE, 'wb') as f:
-        pickle.dump(coeffs, f)
+        pickle.dump(supp_vecs, f)
     f.close()
     with open(MODEL_FILE, 'wb') as f:
-        pickle.dump(svm, f)
+        pickle.dump(lsvm, f)
     f.close()
         
 
